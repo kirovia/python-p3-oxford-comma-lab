ba7997a009cc7aa12e658b3b6347ffce3cb17e3b
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-05-27 18:14:49 -0400

    Passes all tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return 'Please input a valid list'
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[0:-1]) + f', and {items[-1]}'
